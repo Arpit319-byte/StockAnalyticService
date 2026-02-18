108d05d7309ca2145c589516db754486d10381cc
Arpit319-byte <arpitchauhanupal@gmail.com>
2026-02-18 16:27:47 +0530

    first commit

---
+++ src/main/java/com/example/StockAnalyticService/StockAnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockAnalyticService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyticServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockAnalyticService/StockAnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockAnalyticService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
