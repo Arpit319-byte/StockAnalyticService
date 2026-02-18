f9d0c8c3e6bf0f8596c5422150cc2817823a1ca6
Arpit319-byte <arpitchauhanupal@gmail.com>
2026-02-18 22:45:52 +0530

    Added all shared entities of the DataIngestion Service

---
+++ src/main/java/com/example/StockAnalyticService/entity/BaseModel.java
@@ -0,0 +1,46 @@
+package com.example.StockAnalyticService.entity;
+
+import java.time.Instant;
+ 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    private void prePersist(){
+        createdAt=Instant.now();
+        updatedAt=Instant.now();
+    }
+
+    @PreUpdate
+    private void preUpdate(){
+        updatedAt=Instant.now();
+    }
+    
+}
---
+++ src/main/java/com/example/StockAnalyticService/entity/Exchange.java
@@ -0,0 +1,41 @@
+package com.example.StockAnalyticService.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "exchange")
+public class Exchange extends BaseModel{
+    
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="code", nullable = false)
+    private String code;
+
+    @Column(name="country", nullable = false)
+    private String country;
+
+    @Column(name="currency", nullable = false)
+    private String currency;
+
+    @Column(name="open_time", nullable = false)
+    private Instant openTime;
+
+    @Column(name="close_time", nullable = false)
+    private Instant closeTime;
+
+    @Column(name="is_active", nullable = false)
+    private Boolean isActive;
+}
